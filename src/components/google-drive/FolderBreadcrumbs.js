ca517f214f2782fd963510150398092a2509622b
unknown <rohu270700@gmail.com>
2021-03-19 01:20:41 +0530

    minor fixes

--- src/components/google-drive/FolderBreadcrumbs.js
+++ src/components/google-drive/FolderBreadcrumbs.js
@@ -4,7 +4,7 @@ import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { Link } from 'react-router-dom';
 import { ROOT_FOLDER } from '../hooks/useFolder';
 
-const FolderBreadcrumbs = ({ currentFolder, childFolders }) => {
+const FolderBreadcrumbs = ({ currentFolder }) => {
     let path = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER];
     if (currentFolder) path = [...path, ...currentFolder.path];
 
